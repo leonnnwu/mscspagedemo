51ee198205b59dca71a46fc2a47ecb203adfa909
leonnnwu <wuliang0306@gmail.com>
2014-03-15 11:05:52 -0700

    First Commit

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+import dbindexer
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+# django admin
+admin.autodiscover()
+
+# search for dbindexes.py in all INSTALLED_APPS and load them
+dbindexer.autodiscover()
+
+urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    ('^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
